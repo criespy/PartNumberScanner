51b484f941b05ef7f7d7fc7b442d78af9504bf1e
Aries Wibowo <riespratama@gmail.com>
2021-11-11 12:01:50 +0700

    database dummy added

--- scanner/models.py
+++ scanner/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Barang(models.Model):
+    part_number = models.CharField(max_length=18)
+    description = models.CharField(max_length=32)
+    barcode = models.TextField()
+
+    def __str__(self):
+        return self.part_number
