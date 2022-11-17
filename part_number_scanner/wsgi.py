517ceb4d7b2532b5262b4f69b8acd6411b08d7cb
Aries Wibowo <riespratama@gmail.com>
2022-11-17 14:51:26 +0700

    WSGI.py ready for deployment

--- part_number_scanner/wsgi.py
+++ part_number_scanner/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path='/var/www/PartNumberScanner/'
 
 from django.core.wsgi import get_wsgi_application
+
+if path not in sys.path:
+	sys.path.insert(0, path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'part_number_scanner.settings')
 
 application = get_wsgi_application()
+
